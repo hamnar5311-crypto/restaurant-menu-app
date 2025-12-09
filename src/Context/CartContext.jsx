d09907f3a337e382b45e36bbdcd51f6a3081c38a
HamnaRiaz <hamna.r5311@gmail.com>
2025-12-09 10:40:53 -0800

    Add files via upload

---
+++ src/Context/CartContext.jsx
@@ -0,0 +1,21 @@
+import React, { createContext, useState } from "react";
+
+export const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (item) => setCart([...cart, item]);
+
+  const removeFromCart = (index) => {
+    setCart(cart.filter((_, i) => i !== index));
+  };
+
+  const clearCart = () => setCart([]);
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart, clearCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
