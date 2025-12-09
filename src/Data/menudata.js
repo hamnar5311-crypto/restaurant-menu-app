fa5a8ef9ed383bebcdea2af5d591d0c4effbc9cf
HamnaRiaz <hamna.r5311@gmail.com>
2025-12-09 10:42:23 -0800

    Add files via upload

---
+++ src/Data/menudata.js
@@ -0,0 +1,401 @@
+const menuData = [
+  {
+    name: " Regular Pizzas",
+    images: [
+      "/images/pizza8.jpg",
+      "/images/pizza3.jpg",
+      "/images/pizza 16.jpg",
+      "/images/pizza6.jpg",
+      "/images/pizza11",
+
+    ],
+
+    items: [
+      { name: "Chicken Tikka (Small)", price: 500 },
+      { name: "Chicken Tikka (Medium)", price: 900 },
+      { name: "Chicken Tikka (Large)", price: 1450 },
+
+      { name: "Chicken Fajita(Small)", price: 500 },
+      { name: "Chicken Fajita(Medium)", price: 900 },
+      { name: "Chicken Fajita(Large)", price: 1450 },
+
+      { name: "Chicken BBQ(Small)", price: 500 },
+      { name: "Chicken BBQ(Medium)", price: 900 },
+      { name: "Chicken BBQ(Large)", price: 1450 },
+
+      { name: "Tandoori Chicken(Small)", price: 500 },
+      { name: "Tandoori Chicken(Medium)", price: 900 },
+      { name: "Tandoori Chicken(Large)", price: 1450 },
+
+
+      { name: "Cheese Lover(Small)", price: 500 },
+      { name: "Cheese Lover(Medium)", price: 900 },
+      { name: "Cheese Lover(Large)", price: 1450 },
+
+
+      { name: "Chicken Supreme(Small)", price: 500 },
+      { name: "Chicken Supreme(Medium)", price: 900 },
+      { name: "Chicken Supreme(Large)", price: 1400 },
+
+      { name: "Cheese Stick(Small)", price: 500 },
+      { name: "Cheese Stick(Medium)", price: 900 },
+      { name: "Cheese Stick(Large)", price: 1400 },
+
+
+    ],
+
+  },
+
+  {
+    name: " Paratha Roll",
+    images: [
+      "/images/pr5.jpg",
+      "/images/pr3.jpg",
+      "/images/pr2.jpg",
+      "/images/pr4.jpg",
+      "/images/pr6.webp",
+
+    ],
+    items: [
+      { name: "Chapata Roll", price: 500 },
+      { name: "Dhamaka Roll", price: 600 },
+      { name: "Chicken Paratha Roll", price: 300 },
+      { name: "Zinger Paratha Roll", price: 380 },
+      { name: "Chicken Tikka Paratha Roll", price: 350 },
+      { name: "Malai Boti Paratha Roll", price: 380 },
+      { name: "Kabab Paratha Roll", price: 350 },
+
+
+    ],
+  },
+  {
+    name: " Pasta",
+    images: [
+      "/images/pas2.jpg",
+
+      "/images/pas3.webp",
+      "/images/pas5.jpg",
+      "/images/pas6.jpg",
+      "/images/pas4.webp",
+
+    ],
+    items: [
+      { name: "White Sauce Pasta", price: 650 },
+ { name: "Short-cut pasta", price: 500 },
+  { name: "Filled Pasta", price: 600 },
+   { name: "B2B Special Pasta", price: 700 },
+ { name: "Chowmein", price: 600 },
+    ],
+  },
+
+  {
+    name: " Special B2B Pizza",
+    images: [
+      "/images/sp1.webp",
+
+      "/images/sp2.jpg",
+      "/images/sp3.jpg",
+      "/images/sp6.jpg",
+      "/images/sp4.webp",
+
+    ],
+    items: [
+
+      { name: "Crown Crust(Medium)", price: 1200 },
+      { name: "Crown Crust(Large)", price: 1800 },
+      { name: "Crown Crust(XL)", price: 2200 },
+
+      { name: "Kabab Crust(Medium)", price: 1200 },
+      { name: "Kabab Crust(Large)", price: 1800 },
+      { name: "Kabab Crust(XL)", price: 2200 },
+
+      { name: "Malai Boti(Medium)", price: 1200 },
+      { name: "Malai Boti(Large)", price: 1800 },
+      { name: "Malai Boti(XL)", price: 2200 },
+
+      { name: "Donner Pizza(Medium)", price: 1200 },
+      { name: "Donner Pizza(Large)", price: 1800 },
+      { name: "Donner Pizza(XL)", price: 2200 },
+
+      { name: "Cheese Stuff(Medium)", price: 1200 },
+      { name: "Cheese Stuff(Large)", price: 1800 },
+      { name: "Cheese Stuff(XL)", price: 2200 },
+
+
+    ],
+  },
+
+
+  {
+    name: " Fries",
+    images: [
+      "/images/f1.jpg",
+      "/images/f2.webp",
+      "/images/f3.jpg",
+      "/images/f4.webp",
+      "/images/f5.jpg",
+
+    ],
+    items: [
+      { name: "Fries Small", price: 150 },
+      { name: "Fries Large", price: 230 },
+      { name: "Fries Family", price: 320 },
+      { name: "Cheese Fries", price: 350 },
+      { name: "Loaded Fries Half", price: 450 },
+      { name: "Loaded Fries Full", price: 720 },
+      { name: "Nuggets 5pcs", price: 250 },
+      { name: "Nuggets 10pcs", price: 450 },
+
+    ],
+  },
+
+
+
+
+  {
+    name: " Burgers",
+    images: [
+      "/images/bur6.webp",
+
+      "/images/bur3.webp",
+      "/images/bur2.webp",
+      "/images/bur4.jpg",
+      "/images/bur5.jpg",
+    
+
+    ],
+
+    items: [
+      { name: "Shami Burger", price: 200 },
+      { name: "Double Egg Burger", price: 350 },
+      { name: "Pizza Burger", price: 400 },
+      { name: "Chicken Patty Burger", price: 420 },
+      { name: "Tikka Burger", price: 300 },
+      { name: "Grill Burger Jumbo", price: 250 },
+      { name: "Zinger Burger", price: 350 },
+      { name: "Jumbo Zinger Burger", price: 480 },
+      { name: "Tower Burger", price: 420 },
+      { name: "Hot & Spicy Burger", price: 300 },
+      { name: "B2B Special Burger(Molten Cheese)", price: 600 },
+
+    ],
+  },
+
+
+
+  {
+    name: " Sandwich",
+  images: [
+      "/images/sand2.webp",
+      "/images/sand3.webp",
+      "/images/sand5.webp",
+      "/images/sand4.jpg",
+      "/images/sand6.jpg",
+
+    ],
+    items: [
+      { name: "Club Sandwich", price: 270 },
+      { name: "Chicken  Sandwich ($-Slice)", price: 320 },
+      { name: "Grill Sandwich", price: 350 },
+      { name: "Cheese Loaded Sandwich", price: 380 },
+      { name: "B2B Special Sandwich", price: 400 },
+
+    ],
+  },
+
+
+  {
+    name: " Shawarma",
+  images: [
+      "/images/sh2.webp",
+      "/images/sh3.jpg",
+      "/images/sh5.jpg",
+      "/images/sh6.jpg",
+      "/images/sh7.webp",
+
+    ],
+    items: [
+      { name: "Chicken Shawarma Large", price: 180 },
+      { name: "Jumbo Shawarma", price: 220 },
+      { name: "Tikka Shawarma", price: 250 },
+      { name: "Malai Boti Shawarma", price: 320 },
+      { name: "Zinger Shawarma", price: 350 },
+      { name: "Grill Shawarma", price: 280 },
+      { name: "Platter Shawarma", price: 380 },
+      { name: "Grill Platter Shawarma", price: 400 },
+      { name: "Zinger Wrap", price: 300 },
+      { name: "Chicken Wrap", price: 270 },
+      { name: "Malai Boti Wrap", price: 320 },
+      { name: "Chicken Tikka Wrap", price: 300 },
+      { name: "B2B Special Shawarma", price: 430 },
+      { name: " B2B Special Zinger Wrap", price: 450 },
+
+
+    ],
+  },
+
+
+
+
+
+  {
+    name: " BBQ",
+    images: [
+     
+      "/images/bbq6.jpg",
+        "/images/bbq5.webp",
+      "/images/bbq7.jpg",
+      "/images/bbq8.webp",
+      "/images/bbq3.webp",
+
+    ],
+    items: [
+      { name: "Chicken Boti", price: 450 },
+      { name: "Seekh Kebab", price: 400 },
+      { name: "Malai Boti", price: 320 },
+      { name: "Green Boti", price: 320 },
+      { name: "Chicken Kebab", price: 120 },
+      { name: "Chest Piece", price: 350 },
+      { name: "Leg Piece", price: 350 },
+      { name: "Fish BBQ", price: 600 },
+    ],
+  },
+  {
+    name: " Main Dishes",
+     images: [
+     "/images/main2.jpg",
+      "/images/main1.jpg",
+        "/images/main7.jpg",
+      "/images/main4.jpg",
+      
+      "/images/main6.jpg",
+
+    ],
+    items: [
+
+      { name: "B2B Special Mutton Karahi", price: 850 },
+      { name: "Beef Karahi", price: 950 },
+      { name: "Mutton Hara Masala", price: 900 },
+      { name: "Punjabi Mutton Karahi", price: 850 },
+      { name: "Lahori Mutton Karahi", price: 950 },
+      { name: "Mutton Peshawri Karahi", price: 850 },
+
+      { name: "White Chicken Qorma", price: 500 },
+      { name: "Chicken Handi", price: 850 },
+      { name: "Chicken Jalfarazi", price: 950 },
+      { name: "Chicken Makhni Handi", price: 950 },
+      { name: "Chicken Green Chilli", price: 850 },
+      { name: "Chicken Achari Handi", price: 950 },
+      { name: "Chicken Madrasi Handi", price: 850 },
+      { name: "Chicken Ginger", price: 950 },
+
+
+
+
+    ],
+  },
+  {
+    name: " Drinks",
+     images: [
+     "/images/wa1.avif",
+      "/images/wa6.jpg ",
+        "/images/wa3.webp",
+      "/images/wa4.jpg",
+      
+      "/images/wa5.webp",
+
+    ],
+    items: [
+      { name: "Mineral Water", price: 100 },
+      { name: "Sprite", price: 200 },
+      { name: "Coca-Cola", price: 200 },
+      { name: "Pepsi", price: 200 },
+      { name: "7Up", price: 200 },
+      { name: "Marinda", price: 180 },
+
+
+    ],
+  },
+
+
+
+  {
+    name: " Desserts",
+    images: [
+     "/images/des2.webp",
+       "/images/strawberry-layer.jpg",
+       "/images/kheer-bg.png",
+       "/images/des3.webp",
+       
+      "/images/tukry.jpg",
+    ],
+    items: [
+      { name: "Custard", price: 230 },
+      { name: "B2B Special Kheer", price: 300 },
+      { name: "Shahi Tukray", price: 300 },
+      { name: "Brownie with Ice-Cream", price: 350 },
+      { name: "Double Fudge Brownie", price: 450 },
+      { name: "Super Fudge Delight", price: 350 },
+      { name: "Tutti Fruti(Fruit Cocktail)", price: 300 },
+      { name: "Molten Lava Icecream", price: 400 },
+
+
+    ],
+  },
+
+
+  {
+    name: " Sundae",
+    images:[ 
+
+"/images/sund1.jpg",
+"/images/sund2.jpg",
+"/images/sund3.jpg",
+"/images/sund4.jpg",
+"/images/sund5.jpg",
+
+    ],
+
+    items: [
+      { name: "Chocolate Sundae ", price: 300 },
+      { name: " Caramel Sundae ", price: 300 },
+      { name: "Strawberry Sundae", price: 300 },
+      { name: "Mango Sundae", price: 300 },
+      { name: "Blueberry Sundae", price: 300 },
+      { name: " Oreo Sundae", price: 380 },
+      { name: "Nutella Sundae", price: 450 },
+      { name: "Lotus Sundae", price: 450 },
+
+
+
+    ],
+  },
+
+
+
+  {
+    name: " Naan",
+     images: [
+     
+        "/images/n1.webp",
+      "/images/n2.jpg",
+      "/images/n3.webp",
+      "/images/n4.jpg",
+ "/images/n6.jpg",
+    ],
+    items: [
+      { name: "Cheese Naan", price: 150 },
+      { name: "Garlic Naan", price: 100 },
+      { name: "Roghni Naan", price: 90 },
+      { name: "Kalwanji Naan", price: 110 },
+      { name: "Alu Wala Naan", price: 110 },
+      { name: "Sada Roti ", price: 30 },
+    ],
+  },
+
+
+
+
+];
+
+export default menuData;
